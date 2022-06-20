08b5e9b52350b0266258d631eea19ef54c27a94e
Leonardo L Tafarelo <leonardoltafarelo@MacBook-Pro-de-mazza.local>
2022-06-20 02:44:17 -0300

    ajuste

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -5,7 +5,7 @@ import { reactotronRedux } from 'reactotron-redux';
 import AsyncStorage from '@react-native-community/async-storage';
 
 export const reactotronConfig = Reactotron.setAsyncStorageHandler(AsyncStorage) // AsyncStorage would either come from `react-native` or `@react-native-community/async-storage` depending on where you get it from
-    .configure({ host: '192.168.1.4' }) // controls connection & communication settings
+    .configure({ host: 'localhost' }) // controls connection & communication settings
     .useReactNative()
     .use(sagaPlugin())
     .use(reactotronRedux()) // add all built-in react native plugins
--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,6 @@
 import { fetchFactory } from '../utils/fetch';
 const api = fetchFactory('https://api.onbank.digital');
 api.setHeader('Onbank-Api-Access-Key', 'fcd3f2ec-9bed-4f23-b2bb-0e4a8f5c4649');
+api.setHeader('hAuthorization', 'vhewfmweyw4yu7nyxw5bpuw4ft1+hud+dc1rnis4taejc=');
 
 export default api;
